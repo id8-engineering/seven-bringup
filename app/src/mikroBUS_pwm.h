7b7be44a74373510804f74122fb4ab112aad2ca6
Bobo Bäck Engström <bobo@id8-engineering.io>
2026-05-18 09:34:04 +0200

    feat: add pwm test
    Signed-off-by: Bobo Bäck Engström <bobo@id8-engineering.io>

---
+++ app/src/mikroBUS_pwm.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2026 ID8 Engineering AB
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef MIKROBUS_PWM_H_
+#define MIKROBUS_PWM_H_
+
+void test_mikroBUS_pwm(void);
+
+#endif /* MIKROBUS_PWM_H_ */
